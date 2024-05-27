27abbadc8da9f2e26c2692416ae9a71949d3c173
Amador <amasabcar@outlook.com>
2024-05-27 10:23:34 +0200

    Add i18n support and language files

---
+++ src/i18n/ui.ts
@@ -0,0 +1,36 @@
+export const languages = {
+	en: {
+        code: "en",
+        name: "English",
+    },
+	es: {
+        code: "es",
+        name: "Español",
+    },
+	de: {
+        code: "de",
+        name: "Deutsch",
+    },
+};
+
+export const defaultLang = "en";
+
+export const showDefaultLang = false;
+
+export const ui = {
+	en: {
+		"nav.experience": "Experience",
+		"nav.projects": "Projects",
+		"nav.about-me": "About Me",
+	},
+	es: {
+		"nav.experience": "Experiencia",
+		"nav.projects": "Proyectos",
+		"nav.about-me": "Sobre Mi",
+	},
+	de: {
+		"nav.experience": "Erfahrung",
+		"nav.projects": "Projekte",
+		"nav.about-me": "Über Mich",
+	},
+} as const;
