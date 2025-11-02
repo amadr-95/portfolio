8ed0baf749ea1c49a1350f514f4776cf3bedf143
Amador Sabido <amador.sabido@zalando.de>
2025-11-02 20:18:47 +0100

    refactor: move userData to separate file and update imports

---
+++ src/data/userData.ts
@@ -0,0 +1,6 @@
+export const userData = {
+    name: "Amador",
+    mail: "amasabcar@outlook.com",
+    github: "https://github.com/amadr-95",
+    linkedin: "https://www.linkedin.com/in/amadorsabido",
+};
