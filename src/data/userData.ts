17b31f1fb9fec0d11c3d2f5926f9c93c32537725
Amador <amasabcar@outlook.com>
2026-05-09 13:22:51 +0200

    update dependencies, add surname to user data, and modify download filename

--- src/data/userData.ts
+++ src/data/userData.ts
@@ -1,5 +1,6 @@
 export const userData = {
     name: "Amador",
+    surname: "Sabido",
     mail: "amasabcar@outlook.com",
     github: "https://github.com/amadr-95",
     linkedin: "https://www.linkedin.com/in/amadorsabido",
